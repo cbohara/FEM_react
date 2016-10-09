698499db2cc0a19f56c2bd77b551db7761e9c002
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-09 15:59:01 -0700

    added React router and routes

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,14 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-var MyTitle = require('./MyTitle')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Landing = require('./Landing')
+const Search = require('./Search')
+const { Router, Route, hashHistory } = require('react-router')
 
-var MyFirstComponent = () => (
-  <div>
-    <MyTitle title='Whatevs' color='rebeccapurple' />
-    <MyTitle title='LOL' color='papayawhip' />
-    <MyTitle title='OMGLOLWTFBBQ' color='#f06d06' />
-  </div>
+const App = () => (
+  <Router history={hashHistory}>
+    <Route path='/' component={Landing} />
+    <Route path='/search' component={Search} />
+  </Router>
 )
 
-ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
