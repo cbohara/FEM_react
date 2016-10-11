356e40aecdc7d05235800d1cc3821f1275828f7e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-10 19:06:34 -0700

    created header component

--- public/bundle.js
+++ public/bundle.js
@@ -60,7 +60,7 @@
 	var IndexRoute = _require.IndexRoute;
 	var hashHistory = _require.hashHistory;
 
-	var _require2 = __webpack_require__(227);
+	var _require2 = __webpack_require__(228);
 
 	var shows = _require2.shows;
 
@@ -25940,6 +25940,7 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var React = __webpack_require__(1);
+	var Header = __webpack_require__(227);
 
 	var Details = function (_React$Component) {
 	  _inherits(Details, _React$Component);
@@ -25963,15 +25964,7 @@
 	      return React.createElement(
 	        'div',
 	        { className: 'container' },
-	        React.createElement(
-	          'header',
-	          { className: 'header' },
-	          React.createElement(
-	            'h1',
-	            { className: 'brand' },
-	            'svideo'
-	          )
-	        ),
+	        React.createElement(Header, null),
 	        React.createElement(
 	          'div',
 	          { className: 'video-info' },
@@ -26017,6 +26010,49 @@
 
 /***/ },
 /* 227 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var _require = __webpack_require__(160);
+
+	var Link = _require.Link;
+
+
+	var Header = React.createClass({
+	  displayName: 'Header',
+	  render: function render() {
+	    return React.createElement(
+	      'header',
+	      { className: 'header' },
+	      React.createElement(
+	        'h1',
+	        { className: 'brand' },
+	        React.createElement(
+	          Link,
+	          { to: '/', className: 'brand-link' },
+	          'svideo'
+	        )
+	      ),
+	      React.createElement(
+	        'h2',
+	        { className: 'header-back' },
+	        React.createElement(
+	          Link,
+	          { to: '/search' },
+	          'Back'
+	        )
+	      )
+	    );
+	  }
+	});
+
+	module.exports = Header;
+
+/***/ },
+/* 228 */
 /***/ function(module, exports) {
 
 	module.exports = {
